e0eb8573ff6393de66b7cac01ea6ef9470ccd4ff
Grantino Integrated Tech <51379476+ak-wizzy@users.noreply.github.com>
2023-08-10 15:55:37 +0100

    Update .NET-script.cs

--- .NET-script.cs
+++ .NET-script.cs
@@ -7,7 +7,7 @@ string OriginUrl = req.Headers.GetValues ("DISGUISED-HOST").FirstOrDefault ();
 log.Info ("RequestURI org: " + OriginUrl);
 //create response
 var response = req.CreateResponse (HttpStatusCode.MovedPermanently);
-if (OriginUrl.Contains ("wvd.3tallah.com")) {
+if (OriginUrl.Contains ("webavd.grantinointegrated.com")) {
 response.Headers.Location = new Uri ("https://rdweb.wvd.microsoft.com/arm/webclient");
 } else {
 log.Info ("error RequestURI org: " + OriginUrl);
